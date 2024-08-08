279cac411c9b6d33f4ad5ff0a6548a2c7ea55df8
Koki Goudy <kgoudy64@gmail.com>
2024-08-08 15:38:14 -0500

    added react-router-dom

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,17 @@
+/* eslint-disable react/jsx-no-undef */
 /* eslint-disable no-unused-vars */
 import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
