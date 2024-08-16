4b1459a2f184ab112cde2a1b06b9233efae47c2c
Koki Goudy <kgoudy64@gmail.com>
2024-08-16 15:50:50 -0500

    added flowbite-react

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,7 +1,9 @@
 /* eslint-disable react/jsx-no-undef */
 /* eslint-disable no-unused-vars */
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
