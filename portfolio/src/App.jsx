ca14e54cda08e4a7ef4f0e29c4581fe815035b10
Koki Goudy <kgoudy64@gmail.com>
2024-08-20 16:02:35 -0500

    importing pages

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
       </Routes>
     </Router>
   );
